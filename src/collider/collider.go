c74914585cb9dbdd895933d80310444cfb4398ab
unknown <jinxixing210@gmail.com>
2018-05-09 22:30:20 +0800

    collider

--- src/collider/collider.go
+++ src/collider/collider.go
@@ -65,7 +65,7 @@ func (c *Collider) Run(p int, useTls bool) {
 		}
 		server := &http.Server{ Addr: pstr, Handler: nil, TLSConfig: config }
 
-		e = server.ListenAndServeTLS("/cert/cert.pem", "/cert/key.pem")
+		e = server.ListenAndServeTLS("/etc/letsencrypt/live/rtc.intopi.com/fullchain.pem", "/etc/letsencrypt/live/rtc.intopi.com/privkey.pem")
 	} else {
 		e = http.ListenAndServe(pstr, nil)
 	}
